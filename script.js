c58ffb8514f0176cb12d7eaf6d92ff8ec088065a
danilrey <526473+gb96@users.noreply.github.com>
2024-03-02 16:12:51 +0500

    я добавил кнопку перезагрузки и рандомный цвет

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
 
 const container = document.querySelector('div.container');
-//container.setAttribute('style', 'display: flex; width: 640px; flex-wrap: wrap;')
+const res = document.querySelector('.reset');
+let color = `rgba(${Math.floor(Math.random()*100)},${Math.floor(Math.random()*100)},${Math.floor(Math.random()*100)},${Math.random()});`
+
+
 function table() {
     for (let i = 0; i < Math.pow(numb,2); i++) {
         const box = document.createElement('div');
         container.appendChild(box);
         box.addEventListener('mouseover', () => {
-            box.setAttribute('style', 'background-color:rgba(7, 67, 7, 0.805);')
+            box.setAttribute('style', `background-color: ${color}`)
         });
+        box.addEventListener('mouseuot', () => {
+            box.removeAttribute('style', `background-color: ${color}`)
+        });
+        res.addEventListener('click', () => {
+            box.setAttribute('style', 'background-color:green;')
+        });        
 }}
 
-const boxes = document.querySelectorAll('.container > div');
 let numb = 0
 const btn = document.querySelector('button');
 btn.addEventListener('click', () => {
-    numb = +prompt('enter number of grid size (less than 10)')
-    if (numb <= 18) {
-        container.setAttribute("style", `width: ${numb*40}px`)
-    } //else if (numb > 18 || numb <25) {
-      //  boxes.setAttribute('style', 'width: 20; height: 20;')
-       // container.setAttribute("style", `width: ${numb*20}px`)
-    //}; 
+    numb = +prompt('enter number of grid size (less than 19)')
+    if (numb >= 19) {
+        numb = 19
+    }
+    container.setAttribute("style", `width: ${numb*40}px`)
     table();
-}, );
-
-
+});
